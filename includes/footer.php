9c5425076e83bebe5c48f705ca3c1a2b8bd10143
Klas Eskilson <klas.eskilson@gmail.com>
2012-11-29 14:08:26 +0100

    fixade html-markupen lite

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,7 @@
+			<div id="footer">
 
-		</div><!-- #container --> <? // när många filer tillsammans bygger en sida kan det bli svårt att se vilken tagg som hör till vilkven starttagg. detta skrivsätt hjälper till att hålla koll. alltså avslutas här div:en med id container ?>
+			</div>
+		</div><!-- #container -->
+<? // när många filer tillsammans bygger en sida kan det bli svårt att se vilken tagg som hör till vilkven starttagg. detta skrivsätt hjälper till att hålla koll. alltså avslutas här div:en med id container ?>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -4,7 +4,14 @@ ini_set('error_reporting', E_ALL);
 
 include("includes/start.php");
 
-build_header('');
+build_header();
+?>
+
+<div id="content">
+
+</div> <!-- #content -->
+
+<?php
 
 build_footer();
 ?>
