c8084bbe6043b75b922906abd7383a7f6d2d714b
petraohlin <petraohlin8@gmail.com>
2012-11-29 16:05:16 +0100

    Lankar i footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,10 @@
 			<div id="footer" class="wrapper">
-
+				<nav>
+					<a href="#">Villkor</a>
+					<a href="#">Kontakt</a>
+					<a href="#">Facebook</a>
+					<a href="#">Twitter</a>
+				</nav>
 			</div>
 		</div><!-- #container -->
 <? // när många filer tillsammans bygger en sida kan det bli svårt att se vilken tagg som hör till vilkven starttagg. detta skrivsätt hjälper till att hålla koll. alltså avslutas här div:en med id container ?>
