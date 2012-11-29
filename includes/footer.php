ead93e8ec6842bf1f1962ad7374ff16161cd53c9
Carl Englund <englund92@gmail.com>
2012-11-29 16:15:43 +0100

    lankade till twitter

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 					<a href="#">Villkor</a>
 					<a href="#">Kontakt</a>
 					<a href="#">Facebook</a>
-					<a href="#">Twitter</a>
+					<a href="https://twitter.com/lithekoll">Twitter</a>
 				</nav>
 			</div>
 		</div><!-- #container -->
