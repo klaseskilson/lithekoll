94eb3da84c0054058f2b48c18e3245a34a182230
Carl Englund <englund92@gmail.com>
2012-11-29 16:24:21 +0100

    lankade till fejsbok

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 				<nav>
 					<a href="#">Villkor</a>
 					<a href="#">Kontakt</a>
-					<a href="#">Facebook</a>
+					<a href="http://www.facebook.com/groups/lithekoll/ target="_blank">Facebook</a>
 					<a href="https://twitter.com/lithekoll" target="_blank">Twitter</a>
 				</nav>
 			</div>
