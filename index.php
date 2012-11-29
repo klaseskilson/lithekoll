d90bd84ed6775eee78889e588ddd5d56a9aa34db
Klas Eskilson <klas.eskilson@gmail.com>
2012-11-29 15:37:19 +0100

    lade till signup på index

--- index.php
+++ index.php
@@ -7,8 +7,16 @@ include("includes/start.php");
 build_header();
 ?>
 
-<div id="content">
+<div id="content" class="wrapper">
 
+	<form action="signup.php" method="post" class="signup">
+		<input type="text" name="fname" id="fname" placeholder="Förnamn" />
+		<input type="text" name="sname" id="sname" placeholder="Efternamn" />
+		<input type="email" name="email" id="email" placeholder="Email" />
+		<input type="password" name="password" id="password" placeholder="Password" />
+		<input type="password" name="confirm" id="confirm" placeholder="Confirm" />
+		<input type="submit" name="submit" id="submit" value="Bli medlem!" />
+	</form>
 </div> <!-- #content -->
 
 <?php
--- signup.php
+++ signup.php
@@ -17,19 +17,6 @@ build_header();
 
 <div id="content" class="wrapper">
 	<h1>Bli medlem!</h1>
-	<form action="signup.php" method="post">
-		<label for="fname">Förnamn</label>
-		<input type="text" name="fname" id="fname" />
-		<label for="sname">Efternamn</label>
-		<input type="text" name="sname" id="sname" />
-		<label for="email">Email</label>
-		<input type="email" name="email" id="email" />
-		<label for="password">Lösenord</label>
-		<input type="password" name="password" id="password" />
-		<label for="confirm">Bekräfta lösenord</label>
-		<input type="password" name="confirm" id="confirm" />
-		<input type="submit" name="submit" id="submit" value="Bli medlem!" />
-	</form>
 </div> <!-- #content -->
 
 <?php
