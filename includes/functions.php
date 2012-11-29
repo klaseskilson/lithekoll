687a2ec030ad6ce5074b2c313456c9b5da9f275f
Klas Eskilson <klas.eskilson@gmail.com>
2012-11-29 17:29:06 +0100

    gjorde signupformuläret till en funktion

--- includes/functions.php
+++ includes/functions.php
@@ -77,4 +77,16 @@ function encrypt_password($password)
 	return false;
 }
 
+// ehco a signup form
+function signupform()
+{
+	echo '	<form action="signup.php" method="post" class="signup">
+		<input type="text" name="fname" id="fname" placeholder="Förnamn" />
+		<input type="text" name="sname" id="sname" placeholder="Efternamn" />
+		<input type="email" name="email" id="email" placeholder="Email" />
+		<input type="password" name="password" id="password" placeholder="Lösenord" />
+		<input type="password" name="confirm" id="confirm" placeholder="Upprepa lösenord" />
+		<input type="submit" name="submit" id="submit" value="Bli medlem!" />
+	</form>';
+}
 ?>
