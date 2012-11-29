b3c2811c340b95bb365d956c36512a93091e3630
Klas Eskilson <klas.eskilson@gmail.com>
2012-11-29 13:54:28 +0100

    moved the functions

---
+++ includes/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+	alla funktioner yo
+*/
+
+/*
+ * startar html-sidan genom att inkludera header.php
+ * @param 	string 	$before om något ska stå innan huvudtiteln i <title>
+ * @param 	string 	$after 	om något ska stå efter huvudtiteln i <title>
+ * @return 	-				hela sidan echo:as
+ */
+function build_header($before = '', $after = '')
+{
+	include("header.php");
+}
+
+/*
+ * avslutar html-sidan genom att inkludera footer.php
+ * @return 	-				hela sidan echo:as
+ */
+function build_footer()
+{
+	include("footer.php");
+}
+
+
+
+?>
--- includes/start.php
+++ includes/start.php
@@ -5,25 +5,6 @@
 	besökaren.
 */
 
-
-/*
- * startar html-sidan genom att inkludera header.php
- * @param 	string 	$before om något ska stå innan huvudtiteln i <title>
- * @param 	string 	$after 	om något ska stå efter huvudtiteln i <title>
- * @return 	-				hela sidan echo:as
- */
-function build_header($before = '', $after = '')
-{
-	include("header.php");
-}
-
-/*
- * avslutar html-sidan genom att inkludera footer.php
- * @return 	-				hela sidan echo:as
- */
-function build_footer()
-{
-	include("footer.php");
-}
+include("functions.php");
 
 ?>
