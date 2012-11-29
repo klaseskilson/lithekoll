dbf0e75ffa6243709d08951bccb0d79ca675d496
petraohlin <petraohlin8@gmail.com>
2012-11-29 17:23:23 +0100

    Stilmall och block-bakgrund

--- index.php
+++ index.php
@@ -6,17 +6,29 @@ include("includes/start.php");
 
 build_header();
 ?>
+<div id="block">
+	<div  class="wrapper">
+		<div class="alignment">
+			<p>
+				Hejhej här är en spexig text om oss.
+			</p>
+		</div><!-- .alignment -->
+		<div class="alignment">
+			<form action="signup.php" method="post" class="signup">
+				<input type="text" name="fname" id="fname" placeholder="Förnamn" />
+				<input type="text" name="sname" id="sname" placeholder="Efternamn" />
+				<input type="email" name="email" id="email" placeholder="Email" />
+				<input type="password" name="password" id="password" placeholder="Lösenord" />
+				<input type="password" name="confirm" id="confirm" placeholder="Upprepa Lösenord" />
+				<input type="submit" name="submit" id="submit" value="Bli medlem!" />
+			</form>
+		</div><!-- .alignment -->
+	</div><!-- .wrapper -->
+</div><!-- #block -->
 
 <div id="content" class="wrapper">
 
-	<form action="signup.php" method="post" class="signup">
-		<input type="text" name="fname" id="fname" placeholder="Förnamn" />
-		<input type="text" name="sname" id="sname" placeholder="Efternamn" />
-		<input type="email" name="email" id="email" placeholder="Email" />
-		<input type="password" name="password" id="password" placeholder="Lösenord" />
-		<input type="password" name="confirm" id="confirm" placeholder="Upprepa Lösenord" />
-		<input type="submit" name="submit" id="submit" value="Bli medlem!" />
-	</form>
+	
 </div> <!-- #content -->
 
 <?php
