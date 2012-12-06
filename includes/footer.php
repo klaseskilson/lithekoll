8c6bdbfce06a34ec29eb8d3e9923fd3233c5153e
Carl Englund <englund92@gmail.com>
2012-12-06 16:32:26 +0100

    fixade lankar och mellanrumm

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,8 @@
 				<nav>
 					<a href="#">Villkor</a>
 					<a href="#">Kontakt</a>
-					<a href="http://www.facebook.com/groups/lithekoll/ target="_blank">Facebook</a>
-					<a href="https://twitter.com/lithekoll" target=""_blank">Twitter</a>
+					<a href="http://www.facebook.com/groups/lithekoll/" target="_blank">Facebook</a>
+					<a href="https://twitter.com/lithekoll" target="_blank">Twitter</a>
 				</nav>
 			</div>
 		</div><!-- #container -->
@@ -17,5 +17,5 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
 
-	<script type="text/javascript" src="js/script.js" />
+	<script type="text/javascript" src="js/script.js"> </script>
 </html>
