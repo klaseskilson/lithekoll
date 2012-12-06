2469eff97be5d9a055644997cfd3bca7fa2fe48b
Carl Englund <englund92@gmail.com>
2012-12-06 15:40:10 +0100

    massa bajs

--- dashboard.php
+++ dashboard.php
@@ -13,28 +13,54 @@ build_header();
 			Här kan det vara nåt
 		</p>
 	</div>
-	<div class="wrapper contentwrapper">
-		<h1>Kontrollpanel</h1>
-		<p>
-			Här kan det vara nåt
-		</p>
+	<div class="wrapper">
+		<div class = "contentwrapper wrapper-50 fleft">
+		<h1>Transaktioner</h1>
+		
+		<?php transactions(); ?>
 	</div>
-	<div class="wrapper contentwrapper">
-		<h1>Kontrollpanel</h1>
-		<p>
-			Här kan det vara nåt
-		</p>
+		<div class = "contentwrapper wrapper-50 fright">
+			<h1>Diagram</h1>
+		   
+    <script type="text/javascript">
+    if(document.readyState === "complete")
+    {
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+  }
+		 </script>
+		  <div id="chart_div" style="width: 390px; height: 100;"></div>
 	</div>
-	<div class="wrapper contentwrapper">
-		<h1>Kontrollpanel</h1>
-		<p>
-			Här kan det vara nåt
-		</p>
+	</div>
+		<div class="wrapper contentwrapper">
+		<h1>Översikt</h1>
+
+	<p>
+	hefjwfwe
+	</p>	
+
 	</div>
 </div>
 
 
 <?php
 
-build_footer();
+build_footer(true);
 ?>
--- includes/footer.php
+++ includes/footer.php
@@ -3,10 +3,14 @@
 					<a href="#">Villkor</a>
 					<a href="#">Kontakt</a>
 					<a href="http://www.facebook.com/groups/lithekoll/ target="_blank">Facebook</a>
-					<a href="https://twitter.com/lithekoll" target="_blank">Twitter</a>
+					<a href="https://twitter.com/lithekoll" target=""_blank">Twitter</a>
 				</nav>
 			</div>
 		</div><!-- #container -->
-<? // när många filer tillsammans bygger en sida kan det bli svårt att se vilken tagg som hör till vilkven starttagg. detta skrivsätt hjälper till att hålla koll. alltså avslutas här div:en med id container ?>
-	</body>
+
+<?php 
+	if($loadchartapi)
+		echo  '<script type="text/javascript" src="https://www.google.com/jsapi"></script>';
+?>	</body>
+	<script type="text/javascript" src="js/script.js" />
 </html>
