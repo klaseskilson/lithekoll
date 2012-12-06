b3c434ecae0caea2c0496ce407b147773c3fb24b
Carl Englund <englund92@gmail.com>
2012-12-06 09:53:32 +0100

    Lade till en projektsida

---
+++ project.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+
+include("includes/start.php");
+
+build_header();
+?>
+
+	<div class="fancyborder">
+	<div class="wrapper">
+		<div class="alignment">
+			<p>
+				Detta är ett projekt i kursen TNMK30 på Linköpings Universitet
+			</p>
+			<p>
+				LiTHekoll är en tjänst för att hålla koll på sin ekonomi
+			</p>
+			<p>
+				Vi som har gjort sidan är Carl Englund, Klas Eskilsson, 
+				Johan Henriksson, Mattias Palmgren och Petra Öhlin
+			</p>
+		</div><!-- .alignment -->
+		
+	</div><!-- .wrapper -->
+</div><!-- #block -->
+
+<div id="content" class="wrapper">
+</div> <!-- #content -->
+
+
+<?php
+
+build_footer();
+?>
