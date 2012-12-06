31521fabd027da3b321646ab5a22ec3a80529e15
Carl Englund <englund92@gmail.com>
2012-12-06 10:11:11 +0100

    andrade text och la till dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+
+include("includes/start.php");
+
+build_header();
+?>
+
+<p>
+	Här kan det vara nåt
+</p>
+
+
+
+
+
+
+
+
+
+<?php
+
+build_footer();
+?>
