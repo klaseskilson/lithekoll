57b10b67c8cb77bc67336cec07dc574adb8ca7c5
Carl Englund <englund92@gmail.com>
2012-12-09 19:46:18 +0100

    rensade och fixade lite

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 
 (function() {
-$('.focus').click(function() {
+	$('.focus').click(function() {
 	$('.focus').removeClass('show');
-		$('.ofokus').addClass('show');
+	$('.ofokus').addClass('show');
+	$('.utgift').toggle();
 
 
 });
 })();
 
 (function() {
-$('.ofokus').click(function() {
+	$('.ofokus').click(function() {
 	$('.ofokus').removeClass('show');
-		$('.focus').addClass('show');
+	$('.focus').addClass('show');
 	
 
 });
