efa1284bde8cb3e8460040bc67b9cc4d902d29d1
Klas Eskilson <klas.eskilson@gmail.com>
2012-12-10 10:38:27 +0100

    jsuterade stiler lite

--- dashboard.php
+++ dashboard.php
@@ -15,46 +15,46 @@ build_header();
 	</div>
 	<div class="wrapper">
 		<div class = "contentwrapper wrapper-50 fleft">
-		<h1>Transaktioner</h1>
-		
+		<h2>Transaktioner</h2>
+
 		<?php transactions(); ?>
 	</div>
 		<div class = "contentwrapper wrapper-50 fright">
-			<h1>Diagram</h1>
-		   
-      <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]);
+			<h2><?php echo date('Y - m'); ?></h2>
 
-        var options = {
-          title: 'My Daily Activities'
-        };
+			<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+			<script type="text/javascript">
+				google.load("visualization", "1", {packages:["corechart"]});
+				google.setOnLoadCallback(drawChart);
+				function drawChart() {
+					var data = google.visualization.arrayToDataTable([
+						['Kategori', 'Pengar'],
+						['Work',     11],
+						['Eat',      2],
+						['Commute',  2],
+						['Watch TV', 2],
+						['Sleep',    7]
+					]);
 
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>
+					var options = {
+						title: 'Hejhej!'
+					};
 
-     <div id="chart_div" style="width: 350px; height: 300px;"></div>
+					var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+					chart.draw(data, options);
+				}
+			</script>
+
+     <div id="chart_div" style="width: 100%; height: 300px;"></div>
 
 	</div>
 	</div>
 		<div class="wrapper contentwrapper">
-		<h1>Översikt</h1>
+		<h2>Översikt</h2>
 
 	<p>
 	hefjwfwe
-	</p>	
+	</p>
 
 	</div>
 </div>
