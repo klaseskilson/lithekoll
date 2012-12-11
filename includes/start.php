6e8973a2d55321e17f5486490cb7b87be0bd5712
Klas Eskilson <klas.eskilson@gmail.com>
2012-12-11 18:37:10 +0100

    fixade svenska tecken från db

--- includes/start.php
+++ includes/start.php
@@ -1,6 +1,7 @@
 <?php
 // session_start gör så att vi kan hantera användares inloggningar
 session_start();
+
 /*
 	start.php
 	tanken med denna fil är att den ska vara med i varje fil för att starta upp sidan för
@@ -13,6 +14,8 @@ $connection = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)
 	or die("Näru: " . mysql_error());
 $db = mysql_select_db(DB_DATEBASE)
 	or die("Nix: " . mysql_error());
+// sätt rätt teckenkodning för mysqldatabawsen
+mysql_set_charset('utf8',$connection);
 
 // hämta funktioner
 include("functions.php");
